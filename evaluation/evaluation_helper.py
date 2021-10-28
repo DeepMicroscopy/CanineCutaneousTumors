ff8a061354ebf3ce4777fd8e7025bad1b8553ab6
wilm <frauke.wilm@fau.de>
2021-10-28 11:15:20 +0200

    - Updated evaluation modules and slide inference notebook

--- evaluation/evaluation_helper.py
+++ evaluation/evaluation_helper.py
@@ -1,79 +1,107 @@
 import sys
 sys.path.insert(0, '../')
-import h5py
 from torchvision import transforms
 from fastai.vision import *
-import cv2
 from sklearn.metrics import confusion_matrix
+from tqdm import tqdm
+from torch.nn.functional import fold
 
-
-def wsi_inference(slide_container, device, patch_size_s, patch_size_c,level_s,level_c, batch_size, segmentation_learner, classification_learner):
-    factor_s = slide_container.slide.level_downsamples[level_s]
-    factor_c = slide_container.slide.level_downsamples[level_c]
-    shape_s = slide_container.slide.level_dimensions[level_s]
-    shape_c = slide_container.slide.level_dimensions[level_c]
-    segmentation_store = h5py.File("{}_segmentation.hdf5".format(slide_container.file.stem), "w")
-    classification_store = h5py.File("{}_classification.hdf5".format(slide_container.file.stem),"w")
-    segmentation_results = segmentation_store.create_dataset("results",(shape_s[1], shape_s[0]), compression="gzip")
-    classification_results = classification_store.create_dataset("results", (int(np.round(shape_c[1]//patch_size_c)), int(np.round(shape_c[0]//patch_size_c))),compression="gzip")
-
-    x_length = int((shape_s[0] // patch_size_s) + 1)
-    y_length = int((shape_s[1] // patch_size_s) + 1)
-    columns, rows = np.indices((x_length, y_length)).reshape(2, -1) * patch_size_s
-    tumor_class = segmentation_learner.data.classes.index('Tumor')
-
+def segmentation_inference(slide,store, patch_size, level, batch_size, learner, overlap_factor, indices = None):
+    shape = slide.level_dimensions[level]
+    classification_indices = torch.zeros((0,2)).to(learner.data.device)
+    if indices != None:
+        x_indices = indices[:,0]
+        y_indices = indices[:,1]
+    else:
+        x_indices = np.arange(0,int((shape[0] // (patch_size*overlap_factor)) + 1))* int(patch_size * overlap_factor)
+        y_indices = np.arange(0,int((shape[1] // (patch_size*overlap_factor)) + 1))* int(patch_size * overlap_factor)
+    segmentation_results = store.create_dataset("segmentation", (shape[1], shape[0]), compression="gzip")
+    temp = torch.zeros(learner.data.c, int(2*patch_size-overlap_factor*patch_size), shape[0]).to(learner.data.device)
     with torch.no_grad():
         # segmentation inference
-        segmentation_learner.model.eval()
-        indices = np.vstack((columns, rows)).transpose()
-        index_loader = DataLoader(indices, batch_size=batch_size)
-        for index in index_loader:
-            columns, rows = index[:, 0].numpy(), index[:, 1].numpy()
-            batch = torch.stack([transforms.Normalize(*segmentation_learner.data.stats)(
-                pil2tensor(slide_container.get_patch(column, row) / 255., np.float32)) for column, row in
-                                 zip(columns, rows)])
-            seg_preds = segmentation_learner.model(batch.to(device=device))
-            seg_preds = seg_preds.argmax(dim=1)[None]
-            seg_preds = seg_preds.cpu().numpy().squeeze(0)
-            for column, row, seg_pred in zip(columns,rows,seg_preds):
-                stop_y_s = int(shape_s[1] % patch_size_s)
-                stop_x_s = int(shape_s[0] % patch_size_s)
-                if row // patch_size_s != y_length - 1:
-                    stop_y_s = patch_size_s
-                if column // patch_size_s != x_length - 1:
-                    stop_x_s = patch_size_s
-                segmentation_results[int(row):int(row + stop_y_s), int(column):int(column + stop_x_s)] = seg_pred[:stop_y_s,:stop_x_s]
-                # classification inference
-                classification_learner.model.eval()
-                stop_y_c = int((patch_size_s*factor_s)//(patch_size_c*factor_c))
-                stop_x_c = int((patch_size_s*factor_s)//(patch_size_c*factor_c))
-                if column // (patch_size_s) == int(shape_s[0] // (patch_size_s)):
-                    stop_x_c = int(((shape_s[0] % (patch_size_s))*factor_s)//(patch_size_c*factor_c))
-                if row // (patch_size_s) == int(shape_s[1] // (patch_size_s)):
-                    stop_y_c = int(((shape_s[1] % (patch_size_s))*factor_s)//(patch_size_c*factor_c))
-                idxs = np.mgrid[0:stop_x_c,0:stop_y_c:].reshape(2,-1).T * int((patch_size_c*factor_c) // factor_s)
-                seg_outputs = [seg_pred[i[1]:i[1] + int((patch_size_c*factor_c) // factor_s),
-                                i[0]:i[0] + int((patch_size_c*factor_c) // factor_s)] for i in idxs]
-                # only pass patches to classification network, if more than 90% was segmented as tumor
-                idxs = [idxs[i] for i in range(idxs.shape[0]) if
-                        seg_outputs[i].size == int((patch_size_c * factor_c) // factor_s) * int(
-                            (patch_size_c * factor_c) // factor_s) and (
-                                    np.count_nonzero(seg_outputs[i] == tumor_class) / seg_outputs[i].size) > 0.9]
-                classification_loader = DataLoader(idxs, batch_size)
-                for i in classification_loader:
-                    cs,rs = i[:, 0].numpy(), i[:, 1].numpy()
-                    classification_batch = [pil2tensor(np.array(slide_container.slide.read_region(location=(
-                    int((column + c) * factor_s), int((row + r) * factor_s)),level=level_c, size=(
-                        patch_size_c, patch_size_c)))[:, :, :3] / 255., np.float32) for c, r in zip(cs, rs)]
-                    class_batch = torch.stack([transforms.Normalize(*classification_learner.data.stats)(c) for c in classification_batch])
-                    class_pred = torch.softmax(classification_learner.model(class_batch.to(device=device)),1)
-                    classifications = (class_pred).argmax(dim=1)
-                    for c,r,cl in zip(cs,rs,classifications):
-                        classification_results[int(round(((r + row)*factor_s)/factor_c/patch_size_c)),int(round(((c + column)*factor_s)/factor_c/patch_size_c))] = cl.cpu() + 1
-    segmentation_store.close()
-    classification_store.close()
+        learner.model.eval()
+        for y in tqdm(y_indices,desc='Processing %s' % Path(slide._filename).stem):
+            x_loader = DataLoader(x_indices, batch_size=batch_size)
+            row_temp = []
+            for xs in x_loader:
+                input_batch = torch.stack([transforms.Normalize(*learner.data.stats)(pil2tensor(np.array(
+                    slide.read_region(location=(int(x * slide.level_downsamples[level]),
+                                                int(y * slide.level_downsamples[level])),
+                                      level=level, size=(patch_size, patch_size)))[:, :, :3] / 255., np.float32)) for x
+                                           in xs])
+                seg_pred = torch.softmax(learner.model(input_batch.to(device=learner.data.device)),dim=1)
+                row_temp += [s.view(s.shape[0],s.shape[1]*s.shape[2],1) for s in seg_pred]
+            row_output = fold(torch.cat(row_temp, dim=2), (patch_size,int((len(x_indices) + 1) * patch_size * overlap_factor)),
+                 kernel_size=(patch_size, patch_size), stride=int(patch_size * overlap_factor)).squeeze(1)[:,:,:shape[0]]
+            temp[:, int(patch_size * overlap_factor):, :] += row_output
+            temp = temp.roll(-int(patch_size * overlap_factor),dims=1)
+            temp[:, -int(patch_size * overlap_factor):, :] = 0
+            width = segmentation_results[y:int(y + patch_size * overlap_factor),:].shape[0]
+            for x in range(0, int(shape[0] // patch_size + 1)):
+                height = segmentation_results[:, int(x * patch_size):int((x + 1) * patch_size)].shape[1]
+                segmentation_results[
+                y:int(y + patch_size * overlap_factor),
+                int(x * patch_size):int((x + 1) * patch_size)] = temp[:,:width,int(x*patch_size):int(x*patch_size) + height].argmax(dim=0).cpu()
+                classification_indices = torch.cat((classification_indices, (torch.nonzero(
+                    temp[:, :width, int(x * patch_size):int(x * patch_size) + height].argmax(
+                        dim=0) == learner.data.classes.index("Tumor"))+torch.Tensor([y,x*patch_size]).to(learner.data.device))*slide.level_downsamples[level]), dim=0)
+            torch.cuda.empty_cache()
+    return classification_indices
 
-def jaccard_score(cm_matrix, labels):
+
+def classification_inference(slide,store, patch_size,level,batch_size, learner, indices = None):
+    shape = slide.level_dimensions[level]
+    if indices != None:
+        indices = torch.unique(indices//slide.level_downsamples[level]//patch_size, dim=0).cpu().flip(dims=[1])*patch_size
+    else:
+        indices = np.indices((int(shape[0] // patch_size),int(shape[1]//patch_size))).reshape(2,-1).T
+    classification_results = store.create_dataset("classification", (int(shape[1] // patch_size) , int(shape[0] // patch_size)), compression="gzip")
+
+    with torch.no_grad():
+        index_loader = DataLoader(indices, batch_size=batch_size)
+        # classification inference
+        learner.model.eval()
+        for ind in tqdm(index_loader,desc='Processing %s' % Path(slide._filename).stem):
+            input_batch = torch.stack([transforms.Normalize(*learner.data.stats)(pil2tensor(np.array(
+                slide.read_region(location=(int(i[0] * slide.level_downsamples[level]),
+                                            int(i[1] * slide.level_downsamples[level])),
+                                  level=level, size=(patch_size, patch_size)))[:, :, :3] / 255., np.float32)) for i
+                                       in ind])
+            clas_pred = learner.model(input_batch.to(device=learner.data.device))
+            if is_tuple(clas_pred):
+                clas_pred = clas_pred[-1]
+            clas_pred = torch.softmax(clas_pred,dim=1)
+            for j, i in enumerate(ind):
+                try:
+                    classification_results[int(i[1]//patch_size), int(i[0]//patch_size)] = torch.argmax(clas_pred, dim=1)[j].cpu()
+                except:
+                    continue
+
+def segmentation_cm_matrix(slide_container, prediction, classes):
+    cm = np.zeros((classes,classes))
+    x_length = np.arange(int((slide_container.slide_shape[0] // slide_container.width) + 1))*slide_container.width
+    y_length = np.arange(int((slide_container.slide_shape[1] // slide_container.height) + 1))*slide_container.height
+    for x in tqdm(x_length):
+        for y in y_length:
+            pred = prediction["segmentation"][y:y+slide_container.height,x:x+slide_container.width]
+            gt = slide_container.get_y_patch(x, y)[:pred.shape[0],:pred.shape[1]]
+            cm += confusion_matrix(gt.flatten(), pred.flatten(), labels=range(classes))
+    return cm
+
+def classification_cm_matrix(slide_container, prediction, classes):
+    cm = np.zeros((classes,classes))
+    x_length = int(slide_container.slide_shape[0] // slide_container.width)
+    y_length = int(slide_container.slide_shape[1] // slide_container.height)
+    for x in tqdm(range(0,x_length)):
+        for y in range(0,y_length):
+            pred = prediction["classification"][y,x]
+            gt = slide_container.get_y_patch(int(x*slide_container.width), int(y*slide_container.height))
+            gt =  np.unique(gt)[np.argmax(np.unique(gt, return_counts=True)[1])]
+            if (pred != -1 and gt != -1):
+                cm[int(gt),int(pred)] += 1
+    return cm
+
+def slide_jaccard_score(cm_matrix, labels):
     ious = np.zeros((len(labels)))
     ious[:] = np.NAN
     total = cm_matrix.sum()
@@ -96,40 +124,13 @@ def jaccard_score(cm_matrix, labels):
     print("IoUs: ", dict(zip(np.array(labels)[iousValid], np.round(ious[iousValid],4))), "Mean: ", miou)
     print("Frequency-Weighted IoU: ", np.round(fwiou,4))
 
+def slide_tumor_recall(cm_matrix, labels):
+    slide_label = np.argmax(np.sum(cm_matrix[1:,:], axis=1)) + 1
+    print("Slide Label: ", labels[slide_label])
+    print("Slide Classification: ", labels[np.argmax(cm_matrix[slide_label,1:]) + 1])
+    print("Tumor Recall: ", np.round(cm_matrix[slide_label, slide_label] / np.sum(cm_matrix[1:, 1:]), 4))
 
-def slide_cm_matrix(slide_container, classes):
-    try:
-        prediction = h5py.File("{}_segmentation.hdf5".format(slide_container.file.stem), "r")
-    except:
-        print("Please run slide inference first to generate a segmentation output")
-        return
-    gt = -1*np.ones(shape=(slide_container.slide_shape[1], slide_container.slide_shape[0]), dtype=np.int8)
-    inv_map = {v: k for k, v in slide_container.tissue_classes.items()}
-    for poly in slide_container.polygons:
-        coordinates = np.array(poly['segmentation']).reshape((-1,2))/ slide_container.down_factor
-        label = slide_container.label_dict[inv_map[poly["category_id"]]]
-        cv2.drawContours(gt, [coordinates.reshape((-1, 1, 2)).astype(int)], -1, label, -1)
-    white_mask = cv2.cvtColor(np.array(slide_container.slide.read_region(location=(0, 0), level=slide_container._level,
-                                                                         size=(slide_container.slide_shape[0],
-                                                                               slide_container.slide_shape[1])))[:, :,
-                              :3], cv2.COLOR_RGB2GRAY) > slide_container.white
-    excluded = (gt == -1)
-    gt[np.logical_and(white_mask, excluded)] = 0
-    cm = confusion_matrix(gt.flatten(),np.array(prediction["results"]).flatten(),labels=range(classes))
-    return cm
 
-def slide_accuracy(slide_container, labels):
-    try:
-        prediction = h5py.File("{}_classification.hdf5".format(slide_container.file.stem), "r")
-    except:
-        print("Please run slide inference first to generate a classifiation output")
-        return
-    tumors, counts = np.unique(prediction["results"], return_counts=True)
-    counts = counts[tumors != 0]
-    tumors = tumors[tumors != 0]
-    counts = np.round(counts / np.sum(counts), 4)
-    print("Classification Accuracies: ", dict(zip(np.array(labels)[np.array(tumors-1, dtype=int)], counts)))
-    print("Slide Classification: ", np.array(labels)[int(tumors[np.argmax(counts)] - 1)] )
 
 
 
